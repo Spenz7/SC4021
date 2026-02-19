2a99e096b745d5db7436ba99a742f4e207e0f1c6
Spenz7 <msrun3spencer@gmail.com>
2026-02-19 12:16:22 +0800

    update comment

--- 3.1/3.1Step5/fullCrawl.py
+++ 3.1/3.1Step5/fullCrawl.py
@@ -32,6 +32,8 @@ keywords = [
     "automated candidate screening",
     "interview bot",
     "candidate ranking AI",
+    "[MONTHLY MEGATHREAD] AI &amp; Automation in Recruitment: Tools, Trends, and Ethics" 
+    #REV only use the last one for r/recruitment, maybe for r/recruiting if u remove the txt in [ ]
 ]
 
 MIN_COMMENTS = 25
